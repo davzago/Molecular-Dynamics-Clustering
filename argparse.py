4321228baf3a6fe73fff4171da63694e2e89e4be
Stefano <panozzostefano@gmail.com>
2020-06-15 09:48:20 +0200

    Aggiunti altri esempi MD + argparse

---
+++ argparse.py
@@ -0,0 +1,12 @@
+import argparse
+
+### $ python3 stru.py contact_maps_paths.txt -conf params.ini -out_dir results/ -tmp_dir /tmp
+
+parser = argparse.ArgumentParser(description='Hierarchical clustering using RING data.')
+parser.add_argument('data_path', help='File with the path to RING contact map files (edge files)')
+parser.add_argument('-conf', help='Configuration file with algorithm parameters')
+parser.add_argument('-out_dir', help='Output directory')
+parser.add_argument('-tmp_dir', help='Temporary file directory')
+args = parser.parse_args()
+
+print(args.data_path)
