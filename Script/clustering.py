e0ba3161385c77a9723a1136d135e2a88a3eb75c
Davide <zago-7@live.it>
2020-06-24 12:28:02 +0200

    clustering e stampa del dendogramma corretta

--- Script/clustering.py
+++ Script/clustering.py
@@ -5,26 +5,29 @@ from scipy.cluster.hierarchy import dendrogram
 
 def euclidean_cluster(n_cluster, X, out_dir="../fig/"):
     c_type = "euclidean_distance"
-    clustering = AgglomerativeClustering(n_clusters=n_cluster, compute_full_tree=True),fit(X)
+    clustering = AgglomerativeClustering(n_clusters=n_cluster, compute_full_tree=True).fit(X)
     plt.title('Hierarchical Clustering Dendrogram')
     plot_dendrogram(clustering, truncate_mode='level', p=3)
     plt.savefig(out_dir + "dendrogram_" + c_type + ".png")
     plt.show()
     plt.close()
+    return clustering
 
 
 def plot_dendrogram(model, **kwargs):
-    counts = np.zeros(model.children_.shape[0])
-    n_samples = len(model.labels_)
-    for i, merge in enumerate(model.children_):
-        current_count = 0
-        for child_idx in merge:
-            if child_idx < n_samples:
-                current_count += 1  
-            else:
-                current_count += counts[child_idx - n_samples]
-        counts[i] = current_count
 
-    linkage_matrix = np.column_stack([model.children_, model.distances_, counts]).astype(float)
+    # Children of hierarchical clustering
+    children = model.children_
 
+    # Distances between each pair of children
+    # Since we don't have this information, we can use a uniform one for plotting
+    distance = np.arange(children.shape[0])
+
+    # The number of observations contained in each cluster level
+    no_of_observations = np.arange(2, children.shape[0]+2)
+
+    # Create linkage matrix and then plot the dendrogram
+    linkage_matrix = np.column_stack([children, distance, no_of_observations]).astype(float)
+
+    # Plot the corresponding dendrogram
     dendrogram(linkage_matrix, **kwargs)
