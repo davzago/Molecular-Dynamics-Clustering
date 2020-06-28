469626e6714d4fb8e6ef0c50f01b090cb06ba43c
Stefano <panozzostefano@gmail.com>
2020-06-28 19:19:00 +0200

    Salvataggio risultati TMscore su file
    Attenzione, sovrascrive ogni volta "output.txt"

--- Script/RMSD.py
+++ Script/RMSD.py
@@ -3,11 +3,12 @@ import os
 import numpy as np
 
 def get_dense_array(path_to_pdb): # now cycle trough the files with os
-  directory = os.fsencode('../MD_simulations/antibody/pdb')
+  directory = os.fsencode(path_to_pdb)
   files = sorted(os.listdir(directory), key=lambda s : int(s.decode('utf-8').split(".")[1]))
   n = len(files)
   c = 0 # pos of the distance in the distance array
   distance_array = np.zeros(int(n*(n-1)/2)) # using gauss approach i calculate the number of unique pair distances
+  resultFile = open("output.txt","w")
   for i in range(0, n-1):
     for j in range(i+1, n):
       structure1 = files[i].decode('utf-8')
@@ -16,7 +17,10 @@ def get_dense_array(path_to_pdb): # now cycle trough the files with os
                                    path_to_pdb + '/' + structure1,
                                    path_to_pdb + '/' + structure2])
       distance_array[c] = parseRMSD(out)
+      resultFile.write(str(i) + "-" + str(j) + ": " + str(distance_array[c]) + "\n")
       c += 1
+    print(i)
+  resultFile.close()
   return distance_array
 
 def parseRMSD(output):
